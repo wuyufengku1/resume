1958ae3741cea3b4284a664c11417fa45f38b1f9
wuyufengku1 <958038792@qq.com>
2021-04-01 08:26:14 +0800

    first commit

---
+++ precache-manifest.8728f9cb4b9b7aad7c49b1458225da9b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755de48443fa20ccf106f1acd73b2fde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "885634ab4c425b99b93d",
+    "url": "./static/css/2.5c1ee032.chunk.css"
+  },
+  {
+    "revision": "bf369835c8160f19828c",
+    "url": "./static/css/main.a1081557.chunk.css"
+  },
+  {
+    "revision": "885634ab4c425b99b93d",
+    "url": "./static/js/2.38513d6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.38513d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf369835c8160f19828c",
+    "url": "./static/js/main.92464901.chunk.js"
+  },
+  {
+    "revision": "d77098ce7e34cbebdeed",
+    "url": "./static/js/runtime-main.537d6b49.js"
+  },
+  {
+    "revision": "ce32621952b5becc4ed9901f9e23fa22",
+    "url": "./static/media/Netease1.ce326219.png"
+  },
+  {
+    "revision": "82f180a9e106ef14062a04b835a0e796",
+    "url": "./static/media/Netease2.82f180a9.png"
+  },
+  {
+    "revision": "926460e9941b6da94d284d67ff2b23a2",
+    "url": "./static/media/Netease3.926460e9.png"
+  },
+  {
+    "revision": "faf451bb877c869a77b0f0101a1e4b0c",
+    "url": "./static/media/Netease4.faf451bb.png"
+  },
+  {
+    "revision": "58e7909a6be4c6d0d21c1787f6a58c65",
+    "url": "./static/media/NeteaseCloudMusic.58e7909a.png"
+  },
+  {
+    "revision": "905774cdfd96389f1239ac15b13ea239",
+    "url": "./static/media/QRCode.905774cd.png"
+  },
+  {
+    "revision": "3bac19176db6e5ad582ea4d5e8509e86",
+    "url": "./static/media/canvas.3bac1917.png"
+  },
+  {
+    "revision": "3bac19176db6e5ad582ea4d5e8509e86",
+    "url": "./static/media/canvas1.3bac1917.png"
+  },
+  {
+    "revision": "9d114e71293b6f212a61432a1cbf09a3",
+    "url": "./static/media/certificatePhoto.9d114e71.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0e1022f7e391f3669b789016fc02b993",
+    "url": "./static/media/mora.0e1022f7.png"
+  },
+  {
+    "revision": "0e1022f7e391f3669b789016fc02b993",
+    "url": "./static/media/mora1.0e1022f7.png"
+  },
+  {
+    "revision": "2d9c4b5d6dbe62e237f66e526b0f9548",
+    "url": "./static/media/vidly.2d9c4b5d.png"
+  },
+  {
+    "revision": "51b0e9770434329e118b27a554cd313d",
+    "url": "./static/media/vidly1.51b0e977.png"
+  },
+  {
+    "revision": "17aee42720346048c5a9e0694f4861bb",
+    "url": "./static/media/vidly2.17aee427.png"
+  },
+  {
+    "revision": "99a01c3b38e501a1c6847250c88e9dd8",
+    "url": "./static/media/vidly3.99a01c3b.png"
+  },
+  {
+    "revision": "2d3276f3185ddfdf95c8ffc3b384a20e",
+    "url": "./static/media/vidly4.2d3276f3.png"
+  }
+]);
